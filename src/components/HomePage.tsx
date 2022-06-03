0108cef3f1f8f29ef056f55e793e03100d5b493b
o0pmitev <pmitev.dev@gmail.com>
2022-06-03 06:19:03 +0200

    feat: Add styles to main page

--- src/components/HomePage.tsx
+++ src/components/HomePage.tsx
@@ -1,5 +1,8 @@
 import React from 'react';
 import { Link } from "react-router-dom";
+import styled from 'styled-components';
+
+
 import useCompanies from '../hooks/useCompanies';
 
 interface ListContent {
@@ -9,33 +12,85 @@ interface ListContent {
   phone: string;
   image: string;
   email: string;
-  address: string;	
+  address: string;
 }
 
-function HomePage() {
-  const {data, isLoading, isError} = useCompanies();
+interface StyledDlProps {
+  primary?: boolean
+}
 
-  if(isLoading) return <div>Loading ...</div>
-  
-  if(isError) return <div>500 Error</div>
+const SectionContainer = styled.section`
+  width: 100%;
+  font-size: 1.2rem;
+@media (min-width: 1100px) { 
+  width: 90%;
+  margin-inline: auto;
+}
+`;
+
+const ListItem = styled.li`
+  margin-block: .5rem;
+`;
+
+const LinkContainer = styled(Link)`
+  background: #fff;
+  display: block;
+  heigh: 100%;
+  &:hover {
+    background: #f4f4f5;
+    border-radius: 4px;
+  }
+`;
+
+const CompanyContainer = styled.dl<StyledDlProps>`
+padding-inline: 2rem;
+padding-block: 2rem;
+background-color: ${props => props.primary ? "#fff" : "inherit"};
+font-size: ${props => props.primary ? "1.5rem" : "1rem"};
+color: ${props => props.primary ? "#b41d76" : "#575667"};
+text-transform: ${props => props.primary ? "uppercase" : "inherit"};
+font-weight: ${props => props.primary ? "bold" : "normal"};
+  @media (min-width: 550px) {
+    display: flex;
+    flex-direction: row;
+  }
+`;
+
+const CompanyName = styled.dt`
+  width: 10%;
+`;
+const CompanyDescription = styled.dd<StyledDlProps>`
+  margin-inline-start: 10rem;
+`;
+
+function HomePage() {
+  const { data, isLoading, isError } = useCompanies();
+
+  if (isLoading) return <div>Loading ...</div>
+
+  if (isError) return <div>500 Error</div>
 
   console.log(data)
-  const jsx = data.map((company:ListContent) => (
-    <li key={company.id}>
-      <Link to={`/${company.name}`}>
-        <dl>
-          <dt>{company.name}</dt>
-          <dd>{company.description}</dd>
-        </dl>
-      </Link>
-    </li>
+  const jsx = data.map((company: ListContent) => (
+    <ListItem key={company.id}>
+      <LinkContainer to={`/${company.name}`}>
+        <CompanyContainer>
+          <CompanyName>{company.name}</CompanyName>
+          <CompanyDescription>{company.description}</CompanyDescription>
+        </CompanyContainer>
+      </LinkContainer>
+    </ListItem>
 
   ))
-  return <section>
+  return <SectionContainer>
+    <CompanyContainer primary>
+      <CompanyName>name</CompanyName>
+      <CompanyDescription primary>description</CompanyDescription>
+    </CompanyContainer>
     <ul>
       {jsx}
     </ul>
-    </section>
+  </SectionContainer>
 }
 
 export default HomePage;
